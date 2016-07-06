d05bed82652136205462f483afcdc3554a76020d
Jason Gordon <u14405025@tuks.co.za>
2016-07-06 15:46:16 +0200

    Changed the setDate method and removed unused imports

--- src/main/java/me/hyperperform/event/Git/GitEvent.java
+++ src/main/java/me/hyperperform/event/Git/GitEvent.java
@@ -2,18 +2,20 @@ package me.hyperperform.event.Git;
 
 import me.hyperperform.event.Event;
 
-import java.sql.Date;
 import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
+/**
+ * Hyper-perform
+ * Group: CodusMaximus
+ * Date: 2016/07/05
+ * Feature: Github
+ */
 
 public abstract class GitEvent extends Event
 {
 
-	String repoName = null;
-	String user = null;
+	protected String repoName = null;
+	protected String user = null;
 
 	public GitEvent() {}
 	public abstract void CreatePushEvent(String repoName, String date, String user);
@@ -21,23 +23,9 @@ public abstract class GitEvent extends Event
 
 	public abstract void setUser(String user);
 
-	public void setDate(String timestamp)
+	public void setDate(Timestamp timestamp)
 	{
-		DateFormat formatter;
-		formatter = new SimpleDateFormat("dd/MM/yyyy");
-		Date date = null;
-		try
-		{
-			date = (Date) formatter.parse(timestamp);
-		}
-		catch (ParseException e)
-		{
-			e.printStackTrace();
-		}
-
-		Timestamp timeStampDate = new Timestamp(date.getTime());
-
-		time = timeStampDate;
+		time = timestamp;
 	}
 
 }
--- src/main/java/me/hyperperform/event/Git/GitPush.java
+++ src/main/java/me/hyperperform/event/Git/GitPush.java
@@ -1,13 +1,7 @@
 package me.hyperperform.event.Git;
 
-import me.hyperperform.event.Event;
-
 import javax.persistence.*;
-import java.sql.Date;
 import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 /**
  * Hyper-perform
@@ -18,20 +12,18 @@ import java.text.SimpleDateFormat;
 
 @Entity
 @Table
-
 public class GitPush extends GitEvent
 {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-
     private int id;
 
     public GitPush()
     {
     }
 
-    public void CreatePushEvent(String repo, String date, String user)
+    public void CreatePushEvent(String repo, Timestamp date, String user)
     {
         setRepoName(repo);
         setDate(date);
@@ -51,10 +43,9 @@ public class GitPush extends GitEvent
     public void setRepoName(String name)
     {
         repoName = name;
-
     }
 
-    public void setDate(String timestamp)
+    public void setDate(Timestamp timestamp)
     {
         super.setDate(timestamp);
     }

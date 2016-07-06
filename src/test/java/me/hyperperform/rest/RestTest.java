8cf15f8e10a5100c9837dee2e599c88aaab089ee
rohanchhipa <rohan.chhipa@live.com>
2016-07-06 11:57:17 +0200

    Added unit test for git events

--- src/test/java/me/hyperperform/rest/RestTest.java
+++ src/test/java/me/hyperperform/rest/RestTest.java
@@ -1,22 +1,43 @@
 package me.hyperperform.rest;
 
-import org.jboss.resteasy.client.ClientRequest;
-import org.jboss.resteasy.client.ClientResponse;
+import me.hyperperform.listener.GitListener;
+import me.hyperperform.event.MockEvent;
+
+import org.jboss.resteasy.core.Dispatcher;
+import org.jboss.resteasy.plugins.server.resourcefactory.POJOResourceFactory;
+import org.jboss.resteasy.mock.*;
 
 import org.junit.Assert;
+import org.junit.Assert.*;
 import org.junit.Test;
+
+import javax.ws.rs.core.MediaType;
 
 public class RestTest
 {
-	// static String url = "localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/";
-
 	@Test
 	public void simpleTest() throws Exception
 	{
-		// ClientRequest req = new ClientRequest(url + "demo");
-		// ClientResponse<String> res = req.get(String.class);
+		POJOResourceFactory noDef = new POJOResourceFactory(GitListener.class);
+		Dispatcher dispatcher = MockDispatcherFactory.createDispatcher();
 
-		// Assert.assertNotNull(res);
-		// Assert.assertEquals(res, "Hello World!");
+		dispatcher.getRegistry().addResourceFactory(noDef);
+
+		MockHttpRequest request = MockHttpRequest.post("/gitEvent");
+
+		request.header("X-GitHub-Event", "push");
+		request.contentType(MediaType.APPLICATION_JSON_TYPE);
+
+		request.content(MockEvent.gitPushEvent.getBytes());
+
+		MockHttpResponse response = new MockHttpResponse();
+		dispatcher.invoke(request, response);
+
+		System.out.println("-------------------------------------------------");
+		System.out.println(response.getContentAsString());
+		System.out.println(response.getStatus());
+		System.out.println("-------------------------------------------------");
+
+		Assert.assertEquals(response.getStatus(), 200);
 	}
 }

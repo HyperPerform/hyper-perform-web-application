d15c8a3ab28366852c405a0f64d186e401dd54e2
rohanchhipa <rohan.chhipa@live.com>
2016-07-19 20:22:13 +0200

    Added persistence test

---
+++ src/test/java/me/hyperperform/event/PersistenceTest.java
@@ -0,0 +1,37 @@
+package me.hyperperform.event;
+
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ * Created by rohan on 2016/07/19.
+ */
+public class PersistenceTest
+{
+    @Test
+    public void jpaTest()
+    {
+        System.out.println("-------------------------------------------------");
+        System.out.println("Starting JPA test");
+        System.out.println("-------------------------------------------------");
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("test");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+//		GitPush g = new GitPush("baxterthehacker/public-repo", "2015-05-05 19:40:15.0", "baxterthehacker");
+//
+//		entityTransaction.begin();
+//
+//		entityManager.persist(g);
+//
+//		entityTransaction.commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}

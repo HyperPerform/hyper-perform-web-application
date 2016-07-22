01d51e407758d241f631e7882944e20939976d9c
AvinashSingh786 <u14043778@tuks.co.za>
2016-07-22 14:40:49 +0200

    data persistence unit test added

--- src/test/java/me/hyperperform/event/PersistenceTest.java
+++ src/test/java/me/hyperperform/event/PersistenceTest.java
@@ -1,13 +1,10 @@
 package me.hyperperform.event;
 
 import me.hyperperform.event.Git.GitPush;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.*;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 /**
  * Created by rohan on 2016/07/19.
@@ -16,27 +13,66 @@ public class PersistenceTest
 {
 //    @Ignore
 
+    private EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+    private EntityTransaction entityTransaction;
+    private  GitPush g;
+    @Before
+    public void init()
+    {
+        System.out.println("-------------------------------------------------");
+        System.out.println("Starting Persistence Test");
+        System.out.println("-------------------------------------------------");
+        entityManagerFactory = Persistence.createEntityManagerFactory("test");
+        entityManager = entityManagerFactory.createEntityManager();
+        entityTransaction = entityManager.getTransaction();
+        g = new GitPush("baxterthehacker/public-repo", "2015-05-05 19:40:15.0", "baxterthehacker");
+    }
+
     @Test
     public void jpaTest()
     {
-        System.out.println("-------------------------------------------------");
-        System.out.println("Starting JPA test");
-        System.out.println("-------------------------------------------------");
-
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("test");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        EntityTransaction entityTransaction = entityManager.getTransaction();
-
-		GitPush g = new GitPush("baxterthehacker/public-repo", "2015-05-05 19:40:15.0", "baxterthehacker");
+        System.out.print("Starting JPA test ...");
 
 		entityTransaction.begin();
 
 		entityManager.persist(g);
 
 		entityTransaction.commit();
+    }
 
+    @Test
+    public void queryTest()
+    {
+        System.out.print("Starting QueryTest ...");
+
+        Query query = entityManager.createQuery("FROM GitPush", GitPush.class);
+        List<GitPush> result = query.getResultList();
+        Assert.assertNotEquals(result.size(), 0);
+        Assert.assertEquals(g.getRepository(), result.get(result.size()-1).getRepository());
+        Assert.assertEquals(g.getDate(), result.get(result.size()-1).getDate());
+        Assert.assertEquals(g.getUsername(), result.get(result.size()-1).getUsername());
+
+//        System.out.println("ID\t Repository\t\t\t Timestamp\t\t Username");
+
+//        for(int i = 0; i < result.size(); i++)
+//        {
+//            GitPush tmp = result.get(i);
+//            System.out.print(tmp.getId());
+//            System.out.print("\t" + tmp.getRepository());
+//            System.out.print("\t" + tmp.getDate());
+//            System.out.print("\t" + tmp.getUsername());
+//            System.out.println();
+//        }
+
+    }
+
+    @After
+    public void closeManager()
+    {
         entityManager.close();
         entityManagerFactory.close();
-        System.out.println("-------------------   END  -------------------");
+        System.out.println("\t\tSUCCESS");
+
     }
 }

b5fc50287d590f22582092ab58de4abc3754f6ad
JasonDGordon <u14405025@tuks.co.za>
2016-07-24 21:43:48 +0200

    Unit Test for CalendarListener with fixes to CalendarListener & CalendarMeeting

--- src/main/java/me/hyperperform/listener/CalendarListener.java
+++ src/main/java/me/hyperperform/listener/CalendarListener.java
@@ -4,7 +4,6 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.*;
 
 import me.hyperperform.event.Calendar.CalendarMeeting;
-import me.hyperperform.event.Git.GitPush;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -18,46 +17,57 @@ public class CalendarListener implements IListener
 {
     @GET
     @Consumes("application/json")
-    public Response listen(@HeaderParam("X-Goog-Resource-URI") String link, String jsonStr) throws ParseException
+    public Response listen(@HeaderParam("X-Goog-Resource-URI") String link, String jsonStr) throws Exception
     {
+//        System.out.println("Received response from " + link + "\n");
+       try
+       {
+           JSONObject json = (JSONObject) new JSONParser().parse(jsonStr);
+           JSONArray items = (JSONArray) json.get("items");
+           Iterator<JSONObject> iterator = items.iterator();
 
-        System.out.println("Received response from " + link + "\n");
+           while (iterator.hasNext())
+           {
+               JSONObject item = iterator.next();
+               if (item.get("kind").equals("calendar#event"))
+               {
+                   JSONObject crt = (JSONObject) item.get("creator");
+                   JSONObject start = (JSONObject) item.get("start");
+                   JSONArray attendees = (JSONArray) item.get("attendees");
 
-        JSONObject json = (JSONObject)new JSONParser().parse(jsonStr);
+                   ArrayList<String> attendeeEmails = new ArrayList<String>();
 
-        JSONArray items = (JSONArray)json.get("items");
-        Iterator<JSONObject> iterator = items.iterator();
+                   if(attendees != null)
+                   {
+                       Iterator<String> iterate = attendees.iterator();
+                       while (iterate.hasNext())
+                       {
+                           attendeeEmails.add(iterate.next());
+                       }
+                   }
 
-        while(iterator.hasNext())
-        {
-            JSONObject item = iterator.next();
-            if (((JSONObject) item.get("kind")).toString().equals("calendar#event"))
-            {
-                JSONObject crt = (JSONObject) item.get("creator");
-                JSONObject start = (JSONObject) item.get("start");
-                JSONArray attendees = (JSONArray) item.get("attendees");
+                   String eID = ((String) item.get("htmlLink")).split(".eid=")[1];
+                   String creator = (String) crt.get("email");
+                   String cID = link.split("/calendars/")[1].split("/")[0];
+                   String due = (String) start.get("dateTime");
 
-                ArrayList<String> attendeeEmails = new ArrayList<String>();
+                   if(due == null)
+                   {
+                       due = (String) start.get("date");
+                   }
 
-                Iterator<String> iterate = attendees.iterator();
-                while (iterate.hasNext())
-                {
-                    attendeeEmails.add(iterate.next());
-                }
+                   String location = (String) item.get("location");
+                   String timeCreated = (String) item.get("created");
 
-                String eID = ((String) item.get("htmlLink")).split(".eid=")[1];
-                System.out.println("EID: " + eID);
-                String creator = (String) crt.get("email");
-                String cID = link.split("/calendars/")[1].split("/")[0];
-                String due = (String) start.get("dateTime");
-                String location = (String) item.get("location");
-                String timeCreated = (String) item.get("created");
-
-                CalendarMeeting calMeeting = new CalendarMeeting(eID, cID, creator, extractDate(due) + " " + extractTime(due),
-                        location, attendeeEmails, extractDate(timeCreated) + " " + extractTime(timeCreated));
-            }
-        }
-
+                   CalendarMeeting calMeeting = new CalendarMeeting(eID, cID, creator, extractDate(due) + " " + extractTime(due),
+                           location, attendeeEmails, extractDate(timeCreated) + " " + extractTime(timeCreated));
+               }
+           }
+       }
+       catch(Exception e)
+       {
+           e.printStackTrace();
+       }
         return Response.status(200).entity("Successfully received events").header("Access-Control-Allow-Origin", "*").build();
     }
 
@@ -75,18 +85,32 @@ public class CalendarListener implements IListener
 
     private String extractDate(String s)
     {
-        return (s.indexOf('T') != -1) ? s.substring(0, s.indexOf('T')) : s.substring(0, s.indexOf(' '));
+        if(s == null)
+            return null;
+
+        return (s.indexOf('T') != -1 ? s.substring(0, s.indexOf('T')) : s);
     }
 
     private String extractTime(String s)
     {
+        if(s == null)
+            return "";
+
         String tmp = s;
         tmp = s.substring(s.indexOf('T')+1);
+
+        if(tmp.equals(s))
+        {
+            System.out.println();
+            return "00:00:01";
+        }
 
         if (tmp.indexOf('-') != -1)
             tmp = tmp.substring(0, tmp.indexOf('-'));
         else if (tmp.indexOf('+') != -1)
             tmp = tmp.substring(0, tmp.indexOf('+'));
+        else if (tmp.indexOf('.') != -1)
+            tmp = tmp.substring(0, tmp.indexOf('.'));
 
         return tmp;
     }
--- src/test/java/me/hyperperform/rest/RestTest.java
+++ src/test/java/me/hyperperform/rest/RestTest.java
@@ -1,6 +1,7 @@
 package me.hyperperform.rest;
 
 import me.hyperperform.listener.GitListener;
+import me.hyperperform.listener.CalendarListener;
 import me.hyperperform.event.MockEvent;
 
 import org.jboss.resteasy.core.Dispatcher;
@@ -20,7 +21,7 @@ public class RestTest
 	public void simpleTest() throws Exception {
 
 		System.out.println("-------------------------------------------------");
-		System.out.println("Starting REST services test");
+		System.out.println("Starting REST services test - Git push");
 		System.out.println("-------------------------------------------------");
 
 		POJOResourceFactory noDef = new POJOResourceFactory(GitListener.class);
@@ -34,6 +35,31 @@ public class RestTest
 		request.contentType(MediaType.APPLICATION_JSON_TYPE);
 
 		request.content(MockEvent.gitPushEvent.getBytes());
+
+		MockHttpResponse response = new MockHttpResponse();
+		dispatcher.invoke(request, response);
+
+		Assert.assertEquals(response.getStatus(), 200);
+	}
+
+	@Test
+	public void calendarSimpleTest() throws Exception
+	{
+		System.out.println("-------------------------------------------------");
+		System.out.println("Starting REST services test - Calendar");
+		System.out.println("-------------------------------------------------");
+
+		POJOResourceFactory noDef = new POJOResourceFactory(CalendarListener.class);
+		Dispatcher dispatcher = MockDispatcherFactory.createDispatcher();
+
+		dispatcher.getRegistry().addResourceFactory(noDef);
+
+		MockHttpRequest request = MockHttpRequest.get("/calendar");
+
+		request.header("X-Goog-Resource-URI", "https://www.googleapis.com/calendar/v3/calendars/hk3487gd7h57oh9g71he9tfjs0@group.calendar.google.com/events");
+		request.contentType(MediaType.APPLICATION_JSON_TYPE);
+
+		request.content(MockEvent.calendarEvent.getBytes());
 
 		MockHttpResponse response = new MockHttpResponse();
 		dispatcher.invoke(request, response);

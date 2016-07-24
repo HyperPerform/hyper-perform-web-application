610f5a6b3127e9f347f88c97b35229f9d695f364
AvinashSingh786 <u14043778@tuks.co.za>
2016-07-24 21:26:40 +0200

    fixed the naming of the files

--- src/main/java/me/hyperperform/event/IEvent.java
+++ src/main/java/me/hyperperform/event/IEvent.java
@@ -1,7 +1,15 @@
 package me.hyperperform.event;
 
+import java.io.Serializable;
 
-public interface IEvent
+/**
+ * Hyper-perform
+ * Group: CodusMaximus
+ * Date: 2016/07/06
+ * Feature: Event Interface
+ */
+
+public interface IEvent extends Serializable
 {
 
 }

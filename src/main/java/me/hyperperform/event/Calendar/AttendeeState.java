3dfd16cee50ab9a58f79e49b6e4350942d4b562b
AvinashSingh786 <u14043778@tuks.co.za>
2016-07-25 11:15:33 +0200

    Added enum for status types

---
+++ src/main/java/me/hyperperform/event/Calendar/AttendeeState.java
@@ -0,0 +1,12 @@
+package me.hyperperform.event.Calendar;
+
+/**
+ * hyper-perform
+ * Group: CodusMaximus
+ * Date: 2016/07/25
+ * Feature: Attendee State
+ */
+public enum AttendeeState
+{
+    ACCEPTED, TENTATIVE, NEEDSACTION, DECLINED
+}

8c741d08f2fa325624c9504bb43245fd2fdf247d
rohanchhipa <rohan.chhipa@live.com>
2016-07-28 20:27:14 +0200

    javadoc for connection to queue

--- src/main/java/me/hyperperform/QueueConnection.java
+++ src/main/java/me/hyperperform/QueueConnection.java
@@ -12,7 +12,8 @@ import javax.naming.InitialContext;
 import java.io.Serializable;
 
 /**
- * Created by rohan on 2016/07/23.
+ * Provides a connection to the messaging queue handled by application server. Queues are looked up using JNDI lookups.
+ * This components is only used by message producers, i.e the event listeners.
  */
 
 public class QueueConnection
@@ -20,69 +21,62 @@ public class QueueConnection
     @Resource(lookup = "java:/ConnectionFactory")
     private ConnectionFactory connectionFactory;
 
-    // @Resource(lookup = "java:/jms/queue/hyperperform")
-    // private Destination destination;
-
     private Connection connection;
     private Session session;
 
     private MessageProducer messageProducer;
-    private MessageConsumer messageConsumer;
 
+    /**
+     * A method that is called when the class has been constructed. It connects to the messaging queue and retrieves the
+     * destination queue on which objects will be placed.
+     * @throws JMSException
+     */
     @PostConstruct
-    public void initConnection() throws Exception
+    public void initConnection() throws JMSException
     {
        System.setProperty("org.apache.activemq.SERIALIZABLE_PACKAGES","*");
-
-        // System.out.println("-------------------------------------------------");
-        // System.out.println("Connecting to messaging queue");
 
         connection = connectionFactory.createConnection();
         session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         Destination destination = session.createQueue("hyperperform");   
 
         messageProducer = session.createProducer(destination);
-        messageConsumer = session.createConsumer(destination);
-
-        // System.out.println("ConnectionFactory: " + connectionFactory);
-        // System.out.println("Destination: " + destination);
-        // System.out.println("Connection: " + connection);
-        // System.out.println("Session: " + session);
-        // System.out.println("MessageProducer: " + messageProducer);
-        // System.out.println("MessageConsumer: " + messageConsumer);
-        // System.out.println("-------------------------------------------------");
-
-
-        connection.start();
     }
 
+    /**
+     * Called when the object is about to be destroyed. This method ensures that existing connections to the messaging
+     * queue service are closed.
+     * @throws JMSException
+     */
     @PreDestroy
     public void disconnect() throws JMSException
     {
-        connection.stop();
         session.close();
         connection.close();
     }
 
+    /**
+     * Simple method used to place an object onto the queue.
+     * @param event A Serializable object that is to be placed onto the queue.
+     * @throws JMSException
+     */
     public void sendObject(Serializable event) throws JMSException
     {
         messageProducer.send(session.createObjectMessage(event));
     }
 
-    public Serializable receive() throws JMSException
-    {
-        ObjectMessage objectMessage = (ObjectMessage) messageConsumer.receive();
-        return objectMessage.getObject();
-    }
-
+    /**
+     * Getter for the MessageProducer object.
+     * @return Returns instance of a MessageProducer object
+     */
     public MessageProducer getMessageProducer() {
         return messageProducer;
     }
 
-    public MessageConsumer getMessageConsumer() {
-        return messageConsumer;
-    }
-
+    /**
+     * Getter for current session.
+     * @return Returns instance of current session.
+     */
     public Session getSession() {
         return session;
     }

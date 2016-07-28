ff123506a76ec700908c95559545121250051802
rohanchhipa <rohan.chhipa@live.com>
2016-07-28 18:29:02 +0200

    Configuration file for spring

---
+++ src/test/java/me/hyperperform/event/PersistenceTestConfig.java
@@ -0,0 +1,15 @@
+package me.hyperperform.event;
+
+import me.hyperperform.QueueConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by rohan on 2016/07/28.
+ */
+
+@Configuration
+public class PersistenceTestConfig
+{
+
+}

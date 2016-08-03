04a1e8c04d6fc3b3ca1dfd56c7d047e9b458ffed
rohanchhipa <rohan.chhipa@live.com>
2016-08-03 10:01:59 +0200

    Added travis event POJO

---
+++ src/main/java/me/hyperperform/event/Travis/TravisEvent.java
@@ -0,0 +1,80 @@
+package me.hyperperform.event.Travis;
+
+import me.hyperperform.event.Event;
+
+/**
+ * Created by rohan on 2016/08/03.
+ */
+public class TravisEvent implements Event {
+
+    private int id;
+    private String commiter;
+    private String branch;
+    private String repo;
+    private String status;
+    private String timestamp;
+
+    public TravisEvent() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCommiter() {
+        return commiter;
+    }
+
+    public void setCommiter(String commiter) {
+        this.commiter = commiter;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getRepo() {
+        return repo;
+    }
+
+    public void setRepo(String repo) {
+        this.repo = repo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String toString()
+    {
+        String s = "";
+
+        s += "Commiter: " + commiter + "\n";
+        s += "Repo: " + repo + "\n";
+        s += "Branch: " + branch + "\n";
+        s += "Status: " + status + "\n";
+        s += "Timestamp: " + timestamp + "\n";
+
+        return s;
+    }
+}

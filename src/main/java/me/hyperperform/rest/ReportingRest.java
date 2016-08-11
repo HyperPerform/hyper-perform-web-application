7ee37d7b9938bc4a22d20922f1ee5cc47a4eb1e3
rohanchhipa <rohan.chhipa@live.com>
2016-08-11 13:12:36 +0200

    Added reporting rest wrap

---
+++ src/main/java/me/hyperperform/rest/ReportingRest.java
@@ -0,0 +1,38 @@
+package me.hyperperform.rest;
+
+import me.hyperperform.reporting.ReportGenerator;
+import me.hyperperform.reporting.request.*;
+import me.hyperperform.reporting.response.*;
+
+import javax.inject.Inject;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by rohan on 2016/08/10.
+ */
+
+@Path("/report")
+public class ReportingRest {
+
+    @Inject
+    ReportGenerator reportGenerator;
+
+    @GET
+//    @Consumes("application/json")
+//    public Response getSummary(GetSummaryRequest getSummaryRequest)
+    public Response getSummary()
+    {
+        GetSummaryRequest getSummaryRequest = new GetSummaryRequest();
+        getSummaryRequest.setName("baxterthehacker");
+        getSummaryRequest.setStartDate("2015-05-05 10:00:00");
+        getSummaryRequest.setEndDate("2015-05-05 23:00:00");
+
+        GetSummaryResponse getSummaryResponse = reportGenerator.getSummary(getSummaryRequest);
+
+        return Response.status(200).entity(getSummaryResponse).build();
+    }
+}

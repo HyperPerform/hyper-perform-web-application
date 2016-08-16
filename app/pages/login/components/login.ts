974edfe1936bac27c7e6088bc741a86e526faee5
AvinashSingh786 <u14043778@tuks.co.za>
2016-08-16 20:16:35 +0200

    added template files for components

---
+++ app/pages/login/components/login.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+@Component({
+	selector : 'login-cmp',
+	templateUrl : './pages/login/components/login.html',
+	directives: [ROUTER_DIRECTIVES]
+})
+
+export class LoginComponent {}

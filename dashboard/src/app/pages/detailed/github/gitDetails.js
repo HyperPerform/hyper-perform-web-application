aa23e96c9c8e0d4c7f63a6970ca8866c7587d000
Avinash Singh <u14043778@tuks.co.za>
2016-08-27 21:35:45 +0200

    added controller for git graph

---
+++ dashboard/src/app/pages/detailed/github/gitDetails.js
@@ -0,0 +1,24 @@
+
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.detailed.github')
+    .controller('gitGraph', gitGraph);
+
+  function gitGraph($scope) {
+    $scope.labels =["February", "March", "May", "June", "July", "August"];
+    $scope.data = [
+      [65, 59, 90, 81, 56, 55, 40],
+      [28, 48, 40, 19, 88, 27, 45]
+    ];
+    $scope.series = ['Commits', 'Pushes'];
+
+
+  }
+
+
+
+
+
+
+})();

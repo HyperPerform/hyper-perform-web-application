fcbfbaf4d0e5373f68ee806299e7cb6a77111c90
Avinash Singh <u14043778@tuks.co.za>
2016-08-27 10:16:21 +0200

    made the summary rest call

--- dashboard/src/app/pages/dashboard/dashboard.module.js
+++ dashboard/src/app/pages/dashboard/dashboard.module.js
@@ -8,6 +8,7 @@
  var dashboard = angular.module('BlurAdmin.pages.dashboard', ['ui.bootstrap', 'highcharts-ng'])
       .config(routeConfig).controller('pacrtl', loadPa);
 
+
   /** @ngInject */
   function routeConfig($stateProvider) {
     $stateProvider
@@ -22,57 +23,14 @@
         });
   }
 
-   function loadPa($scope) {
+   function loadPa($scope, $http) {
+       // alert("wowowowoowwo");
+       $http.get("http://localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/report/getSummary")
+           .then(function(response) {
+               $scope.summary = response.data;
+            // alert(response.data);
+           });
 
-       $scope.chart = {
-           options: {
-               chart: {
-                   type: 'bar'
-               }
-           },
-           series: [{
-               data: [10, 15, 12, 8, 7]
-           }],
-           title: {
-               text: 'Hello'
-           }
-       };
-        // $scope.addPoints = function () {
-        //
-        //     var seriesArray = $scope.chart.series;
-        //     var rndIdx = Math.floor(Math.random() * seriesArray.length);
-        //     seriesArray[rndIdx].data = seriesArray[rndIdx].data.concat([1, 10, 20])
-        // };
-        //
-        // $scope.addSeries = function () {
-        //     var rnd = [];
-        //
-        //     for (var i = 0; i < 10; i++) {
-        //         rnd.push(Math.floor(Math.random() * 20) + 1)
-        //     }
-        //     $scope.chart.series.push({
-        //         data: rnd
-        //     })
-        // };
-        //
-        // $scope.removeRandomSeries = function () {
-        //     var seriesArray = $scope.chart.series;
-        //     var rndIdx = Math.floor(Math.random() * seriesArray.length);
-        //     seriesArray.splice(rndIdx, 1)
-        // };
-        //
-        // $scope.options = {
-        //     type: 'line'
-        // };
-        //
-        // $scope.swapChartType = function () {
-        //     if (this.chart.options.chart.type === 'line') {
-        //         this.chart.options.chart.type = 'bar'
-        //     } else {
-        //         this.chart.options.chart.type = 'line'
-        //     }
-        // };
-        //
 
 
     }

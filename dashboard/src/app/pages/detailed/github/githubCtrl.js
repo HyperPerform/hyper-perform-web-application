7d9cbd72750379c9b864b4fefbdfe00f28e7c689
Avinash Singh <u14043778@tuks.co.za>
2016-08-27 19:19:57 +0200

    now pulls from server and adds some mock data

--- dashboard/src/app/pages/detailed/github/githubCtrl.js
+++ dashboard/src/app/pages/detailed/github/githubCtrl.js
@@ -3,7 +3,54 @@
 
   angular.module('BlurAdmin.pages.detailed.github').controller('githubCtrl', githubCtrl);
 
-  function githubCtrl($scope) 
-  {}
+  function githubCtrl($scope, $http)
+  {
+    $scope.gitDataSize = 10;
+    var git = [
+        {
+            "id": 1,
+            "username": "Avinash",
+            "repository": "COS332",
+            "date": "2016-02-19",
+            "commitSize": 50
+        },
+        {
+            "id": 2,
+            "username": "Wad",
+            "repository": "COS332",
+            "date": "2016-02-19",
+            "commitSize": 2
+        },
+        {
+            "id": 3,
+            "username": "Brad",
+            "repository": "COS332",
+            "date": "2016-02-19",
+            "commitSize": 20
+        }
+        ,{
+            "id": 4,
+            "username": "Avinash",
+            "repository": "COS332",
+            "date": "2016-02-19",
+            "commitSize": 500
+        }
+    ];
+
+    $http.get("http://localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/report/getDetails")
+        .then(function(response){
+            $scope.gitData = response.data.gitDetails.data[0];
+              // alert(response.data.gitDetails.data[0][0].repository);
+
+
+        });
+        git.push($scope.gitData);
+
+      $scope.gitData = git;
+      //   alert($scope.gitData );
+      //
+
+      // alert($scope.gitData );alert(github );
+  }
 
 })();

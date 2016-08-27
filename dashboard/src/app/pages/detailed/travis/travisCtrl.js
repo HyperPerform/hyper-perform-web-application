8d36917408da5849bb846c34c8e802c63ad04d20
rohanchhipa <rohan.chhipa@live.com>
2016-08-27 14:02:43 +0200

    now requests data from hyperperform server

--- dashboard/src/app/pages/detailed/travis/travisCtrl.js
+++ dashboard/src/app/pages/detailed/travis/travisCtrl.js
@@ -1,9 +1,12 @@
-(function () {
-  'use strict';
-
   angular.module('BlurAdmin.pages.detailed.travis').controller('travisCtrl', travisCtrl);
 
-  function travisCtrl($scope) 
-  {}
+  function travisCtrl($scope, $http) 
+  {
+  	var travisData;
 
-})();
+  	$http.get("http://localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/report/getDetails")
+  	.then(function(response){
+  		travisData = response.data.travisDetails;
+  		$scope.repos = travisData.data;
+  	});
+  }

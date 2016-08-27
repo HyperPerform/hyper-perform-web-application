26532c03579ab2065495424d11507fcec1f8e5a5
Avinash Singh <u14043778@tuks.co.za>
2016-08-27 10:14:46 +0200

    added controller for the PA chart

---
+++ dashboard/src/app/pages/dashboard/chartJs2DCtrl.js
@@ -0,0 +1,28 @@
+/**
+ * @author a.demeshko
+ * created on 12/16/15
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.dashboard')
+    .controller('chartJs2DCtrl', chartJs2DCtrl);
+
+  /** @ngInject */
+  function chartJs2DCtrl($scope) {
+    $scope.labels =["May", "June", "Jule", "August", "September", "October", "November"];
+    $scope.data = [
+      [65, 59, 90, 81, 56, 55, 40],
+      [28, 48, 40, 19, 88, 27, 45]
+    ];
+    $scope.series = ['Commits', 'Pushes'];
+
+
+  }
+
+
+
+
+
+
+})();

8941bf609c858bae2fdb5be550f11f73f25fc1a2
rohanchhipa <rohan.chhipa@live.com>
2016-08-29 22:09:31 +0200

    makes request for issue data

--- src/app/pages/detailed/issues/issuesCtrl.js
+++ src/app/pages/detailed/issues/issuesCtrl.js
@@ -1,9 +1,9 @@
-(function () {
-  'use strict';
+angular.module('BlurAdmin.pages.detailed.issues').controller('issuesCtrl', issuesCtrl);
 
-  angular.module('BlurAdmin.pages.detailed.issues').controller('issuesCtrl', issuesCtrl);
-
-  function issuesCtrl($scope) 
-  {}
-
-})();
+function issuesCtrl($scope, $http) 
+{
+  	$http.get("http://localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/report/getDetails")
+  	.then(function(response){
+  		$scope.repos = response.data.gitIssueDetails.data;
+  	});
+}

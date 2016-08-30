cd3e7c69d187e25e00f3ec146a1648a46b91d82c
rohanchhipa <rohan.chhipa@live.com>
2016-08-30 09:41:41 +0200

    Now make post requests to server

--- src/app/pages/detailed/issues/issuesCtrl.js
+++ src/app/pages/detailed/issues/issuesCtrl.js
@@ -2,8 +2,21 @@ angular.module('BlurAdmin.pages.detailed.issues').controller('issuesCtrl', issue
 
 function issuesCtrl($scope, $http) 
 {
-  	$http.get("http://localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/report/getDetails")
-  	.then(function(response){
+  	// $http.get("http://localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/report/getDetails")
+  	// .then(function(response){
+  	// 	$scope.repos = response.data.gitIssueDetails.data;
+  	// });
+
+  	$http({
+		url: "http://localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/report/getDetails", 
+		method: "POST",
+		data: JSON.stringify({name: "Rohan", startDate: "2016-01-01 00:00:01", endDate: "2016-12-30 23:59:59", type: "issues"}),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+	.then(function(response){
+  		// travisData = response.data.travisDetails;
   		$scope.repos = response.data.gitIssueDetails.data;
   	});
 }
--- src/app/pages/detailed/travis/travisCtrl.js
+++ src/app/pages/detailed/travis/travisCtrl.js
@@ -1,11 +1,29 @@
   angular.module('BlurAdmin.pages.detailed.travis').controller('travisCtrl', travisCtrl);
 
-  function travisCtrl($scope, $http) 
+  function travisCtrl($scope, $http, $window) 
   {
   	var travisData;
 
-  	$http.get("http://localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/report/getDetails")
-  	.then(function(response){
+ //  	var config = {
+	// 	// params: {name: "Rohan", startDate: "2016-01-01 00:00:01", endDate: "2016-12-30 23:59:59", type: "travis"},
+	// 	// headers : {'Content-Type' : 'application/x-www-form-urlencoded'}
+	// };
+
+ //  	$http.get("http://localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/report/getDetails", config)
+ //  	.then(function(response){
+ //  		travisData = response.data.travisDetails;
+ //  		$scope.repos = travisData.data;
+ //  	});
+  	
+  	$http({
+		url: "http://localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/report/getDetails", 
+		method: "POST",
+		data: JSON.stringify({name: "Rohan", startDate: "2016-01-01 00:00:01", endDate: "2016-12-30 23:59:59", type: "travis"}),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+	.then(function(response){
   		travisData = response.data.travisDetails;
   		$scope.repos = travisData.data;
   	});

f0c6bea6350f25b998e07d12de43367b76744c94
Avinash Singh <u14043778@tuks.co.za>
2016-09-01 11:18:50 +0200

    added error icon if failed to access the server

--- src/app/pages/dashboard/dashboard.module.js
+++ src/app/pages/dashboard/dashboard.module.js
@@ -25,11 +25,14 @@
 
    function loadPa($scope, $http) {
        //
+       $scope.p  = "";
        $http.get("http://localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/report/getSummary")
            .then(function(response) {
                $('.fa-spinner, .fa-spin').fadeOut(500, function(){ $(this).remove();});
                $scope.summary = response.data;
+               $scope.p  = "%";
            }, function(response){
+               $('.fa-spinner, .fa-spin').toggleClass().html("<i class='fa fa-exclamation-triangle' style='color: #F0AD4E'></i>");
 
            });
 

bb2116977036bcb68cd26172c411534edfc5c3e2
Avinash Singh <u14043778@tuks.co.za>
2016-09-02 11:07:59 +0200

    added notification modules

---
+++ src/app/pages/notifications/notifications.module.js
@@ -0,0 +1,26 @@
+/**
+ * @author v.lugovsky
+ * created on 16.12.2015
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.notifications', [])
+      .config(routeConfig);
+
+  /** @ngInject */
+  function routeConfig($stateProvider) {
+    $stateProvider
+        .state('notifications', {
+          url: '/notifications',
+          templateUrl: 'app/pages/notifications/notifications.html',
+          controller: 'NotificationsPageCtrl',
+          title: 'Notifications',
+          // abstract: true,
+          sidebarMeta: {
+            order: 150
+          }
+        });
+  }
+
+})();

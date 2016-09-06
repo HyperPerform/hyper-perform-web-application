fcbe9927415dabf3b9ad44e7320a16e9fe185a8b
rohanchhipa <rohan.chhipa@live.com>
2016-09-06 18:14:21 +0200

    constructs graphs per repo

--- src/app/pages/detailed/github/gitDetails.js
+++ src/app/pages/detailed/github/gitDetails.js
@@ -5,15 +5,24 @@
   angular.module('HyperPerform.pages.detailed.github')
     .controller('gitGraph', gitGraph);
 
-  function gitGraph($scope) {
-    $scope.labels =["February", "March", "May", "June", "July", "August"];
-    $scope.data = [
-      [65, 59, 90, 81, 56, 55, 40],
-      [28, 48, 40, 19, 88, 27, 45]
-    ];
-    $scope.series = ['Commits', 'Pushes'];
-    $scope.color = ["#ff5252", "#ffffff"];
+  function gitGraph($scope, $window) {
+    
+    // $scope.labels =["February", "March", "May", "June", "July", "August"];
+    // $scope.data = [
+    //   [65, 59, 90, 81, 56, 55, 40],
+    //   [28, 48, 40, 19, 88, 27, 45]
+    // ];
+    // $scope.series = ['Commits', 'Pushes'];
+    // $scope.color = ["#ff5252", "#ffffff"];
 
+    // $window.alert($scope.$index);
+
+    $scope.labels = $scope.graphs[$scope.$index].independent;
+    $scope.data = [
+      $scope.graphs[$scope.$index].dependent
+    ];
+    $scope.series = ['Commits'];
+    $scope.color = ["#ff5252"];
 
   }
 

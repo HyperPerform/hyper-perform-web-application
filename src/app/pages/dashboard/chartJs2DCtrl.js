75c00568ae2f6dee8b58fcf111a0cc13c1cbe67f
AvinashSingh786 <AvinashSingh786>
2016-09-24 20:13:56 +0200

    added structure and colors for the summary graph

--- src/app/pages/dashboard/chartJs2DCtrl.js
+++ src/app/pages/dashboard/chartJs2DCtrl.js
@@ -10,12 +10,15 @@
 
   /** @ngInject */
   function chartJs2DCtrl($scope) {
-    $scope.labels =["May", "June", "Jule", "August", "September", "October", "November"];
+    $scope.labels =["January", "February", "March", "April", "May", "June", "July"];
     $scope.data = [
       [65, 59, 90, 81, 56, 55, 40],
-      [28, 48, 40, 19, 88, 27, 45]
+      [28, 48, 40, 19, 88, 27, 45],
+      [25, 41, 22, 60, 50, 31, 55],
+      [30, 20, 33, 40, 62, 53, 42]
     ];
-    $scope.series = ['Commits', 'Pushes'];
+    $scope.series = ["Github", "Travis", "Issues", "Entry/Exit" ];
+    $scope.color = ["#40AFEF","#8bc34a","#FF7043","#7D9EAF"];
 
 
   }

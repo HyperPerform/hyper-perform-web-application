6395237c6bbfed6525ff65b18b1f0f2b282c6535
AvinashSingh786 <u14043778@tuks.co.za>
2016-10-03 22:34:44 +0200

    now loads data from the bounded times

--- src/app/pages/dashboard/dashboard.module.js
+++ src/app/pages/dashboard/dashboard.module.js
@@ -2,7 +2,7 @@
 
 
     var dashboard = angular.module('HyperPerform.pages.dashboard', ['ui.bootstrap', 'highcharts-ng', 'angular-svg-round-progressbar'])
-      .config(routeConfig).controller('pacrtl', loadPa);
+      .config(routeConfig).controller('pacrtl', loadPa).controller('modal', ModalsPageCtrl);
 
   /** @ngInject */
   function routeConfig($stateProvider) {
@@ -34,61 +34,64 @@
            }
            return "";
        }
-       // if (document.cookie == "" || document.cookie == null)
-       //     window.location.href = '/';
-       //
-       // var name = document.cookie.split("=");
-       //  name = name[1];
-       //  name = name.split("#")[0];
-       //  // alert(name);
-       //  if (name == "" || name == null)
-       //      window.location.href = '/';
-       //
-       // alert(document.cookie);
 
-       var n = getCookie('hpkey').split("#")[0];
-       // alert(n);
-       // alert(JSON.stringify({name: n, startDate: "2006-01-01 00:00:01", endDate: "2016-12-30 23:59:59"}));
-       $scope.p  = "";
-       $scope.h = " H";
-       $http({
-          method: "POST",
-          url: "http://localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/report/getSummary",
-          data: JSON.stringify({name: n, startDate: "2006-01-01 00:00:01", endDate: "2016-12-30 23:59:59"}),
-           headers: {
-               "Content-Type": "application/json",
-               "Access-Control-Allow-Origin": "*"
-           }
-       })
-           .then(function(response) {
-               $('.fa-spinner, .fa-spin').fadeOut(500, function(){ $(this).remove();});
-               $scope.summary = response.data;
-               // alert(JSON.stringify($scope.summary));
-               $scope.p  = "%";
-           }, function(response){
-               $('.fa-spinner, .fa-spin').toggleClass().html("<i class='fa fa-exclamation-triangle' style='color: #F0AD4E'></i>");
-               $scope.openToast('From: Summary','Unable to connect to the server','error');
-           });
+       $scope.loadDashboard = function() {
 
-       // $http({
-       //     url: "http://localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/login/verifyDetails",
-       //     method: "POST",
-       //     data: JSON.stringify({userEmail: "admin@hyperperform.me", userPassword: "hashedPass"}),
-       //     headers: {
-       //         "Content-Type": "application/json",
-       //         "Access-Control-Allow-Origin": "*",
-       //         'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
-       //         'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With'
-       //
-       //     }
-       // })
-       //     .then(function (response) {
-       //         // alert(JSON.stringify(response.data));
-       //         alert(response.data.loggedin);
-       //         // window.location.href='/user';
-       //     });
+           var t1 = $('#time1').html();
+           var t2 = $('#time2').html();
+           var d1 = $('#date1').html().trim();
+           var d2 = $('#date2').html().trim();
+           var n = getCookie('hpkey').split("#")[0];
+            // alert(JSON.stringify({name: n, startDate: d1 + t1, endDate: d2 + t2}));
+           $scope.p = "";
+           $scope.h = " H";
+           $http({
+               method: "POST",
+               url: "http://localhost:8080/hyperperform-system-1.0-SNAPSHOT/rs/report/getSummary",
+               data: JSON.stringify({name: n, startDate: d1 + t1, endDate: d2 + t2}),
+               headers: {
+                   "Content-Type": "application/json",
+                   "Access-Control-Allow-Origin": "*"
+               }
+           })
+               .then(function (response) {
+                   $('.fa-spinner, .fa-spin').fadeOut(500, function () {
+                       $(this).remove();
+                   });
+                   $scope.summary = response.data;
+                   // alert(JSON.stringify($scope.summary));
+                   $scope.p = "%";
+               }, function (response) {
+                   $('.fa-spinner, .fa-spin').toggleClass().html("<i class='fa fa-exclamation-triangle' style='color: #F0AD4E'></i>");
+                   $scope.openToast('From: Summary', 'Unable to connect to the server', 'error');
+               });
 
+           $('#myModal').modal('hide');
 
+       };
+
+       setTimeout(
+           function()
+           {
+
+               $scope.loadDashboard();
+           }, 1000);
+
+    }
+
+    function ModalsPageCtrl($scope, $uibModal) {
+        $scope.open = function (page, size) {
+            $uibModal.open({
+                animation: true,
+                templateUrl: page,
+                size: size,
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
+        };
     }
 
 })();
